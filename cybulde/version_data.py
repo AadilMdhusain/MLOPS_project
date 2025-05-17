6315d0eb7387f1e286562496587670c0feab4723
Aadil Husain <aadilmohammadhusain@gmail.com>
2025-05-17 15:41:00 +0530

    added functionality to initialize DVC Storage

--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -2,12 +2,14 @@ from pathlib import Path  # 'pathlib.Path' imported but unused
 
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
-from cybulde.utils.data_utils import initialize_dvc
+from cybulde.utils.data_utils import initialize_dvc, initialize_dvc_storage
 from cybulde.utils.utils import get_logger  # imported but unused
 
 @get_config(config_path="../configs", config_name="config")
 def version_data(config: Config) -> None:  # "config" is not accessed
     initialize_dvc()
+
+    initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
 
 if __name__ == "__main__":
     version_data()  # type: ignore
